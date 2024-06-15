9d3caa5cbf4181ecb8a841b15338fd6e2de813ba
akio <akio@rittai.org>
2024-06-15 16:32:14 +0900

    littlefs does not work

---
+++ code/lib/littlefs/example/littlefs_ex.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
+#include "littlefs.h"
+
+lfs_t lfs;
+lfs_file_t file;
+
+void task_lfs(void *) {
+	int8_t err = lsf_mount(&lfs, &lfs_pico_cfg);
+
+	if (err) {
+		lfs_format(&lfs, &lfs_pico_cfg);
+	}
+	uint32_t boot_count = 0;
+	lfs_file_open(&lfs, &file, "boot_count", LFS_O_RDWR | LFS_O_CREAT);
+    lfs_file_read(&lfs, &file, &boot_count, sizeof(boot_count));
+
+    // update boot count
+    boot_count += 1;
+    lfs_file_rewind(&lfs, &file);
+    lfs_file_write(&lfs, &file, &boot_count, sizeof(boot_count));
+
+    // remember the storage is not updated until the file is closed successfully
+    lfs_file_close(&lfs, &file);
+
+    // release any resources we were using
+    lfs_unmount(&lfs);
+
+    // print the boot count
+    printf("boot_count: %d\n", boot_count);
+
+	while (1) vTaskDelay(1000);
+}
+
+int main(void) {
+	stdio_init_all();
+	sleep_ms(2000);
+	printf("%s", __FILE_NAME__);
+	
+	xTaskCreate(task_lfs,"task_lfs",256,NULL,1,NULL);
+    vTaskStartScheduler();
+	while (1) {}
+}
